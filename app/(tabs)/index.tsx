80e175de90a6024782e58d75d5e74adcdeb4ae84
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2026-04-03 17:25:05 -0400

    Created all pakemons card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,49 @@
 import PokemonCard from "@/components/PokemonCard";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 
 export default function App() {
-  const charmanderData = {
-    name: "Charmander",
-    image: require("../../assets/images/charmander.png"),
-    type: "Fire",
-    hp: 39,
-    moves: ["Scratch", "Ember", "Growl", "Leer"],
-    weaknesses: ["Water", "Rock"],
-  };
+  const pokemons = [
+    {
+      name: "Charmander",
+      image: require("../../assets/images/charmander.png"),
+      type: "Fire",
+      hp: 39,
+      moves: ["Scratch", "Ember", "Growl", "Leer"],
+      weaknesses: ["Water", "Rock"],
+    },
+
+    {
+      name: "Squirtle",
+      image: require("../../assets/images/squirtle.png"), // Replace with the actual image path
+      type: "Water",
+      hp: 44,
+      moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+      weaknesses: ["Electric", "Grass"],
+    },
+
+    {
+      name: "Bulbasaur",
+      image: require("../../assets/images/bulbasaur.png"), // Replace with the actual image path
+      type: "Grass",
+      hp: 45,
+      moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+      weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+    },
+    {
+      name: "Pikachu",
+      image: require("../../assets/images/pikachu.png"), // Replace with the actual image path
+      type: "Electric",
+      hp: 35,
+      moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+      weaknesses: ["Ground"],
+    },
+  ];
   return (
-    <View style={styles.container}>
-      <PokemonCard {...charmanderData} />
-    </View>
+    <ScrollView style={styles.container}>
+      {pokemons.map((pokemon) => (
+        <PokemonCard key={pokemon.name} {...pokemon} />
+      ))}
+    </ScrollView>
   );
 }
 
--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,4 +1,4 @@
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
 
 interface PokemonCardProps {
   name: string;
@@ -9,6 +9,21 @@ interface PokemonCardProps {
   weaknesses: string[];
 }
 
+const getTypeDetails = (type: string) => {
+  switch (type.toLowerCase()) {
+    case "electric":
+      return { borderColor: "#FFD700", emoji: "⚡️" };
+    case "water":
+      return { borderColor: "#6493EA", emoji: "💧" };
+    case "fire":
+      return { borderColor: "#FF5733", emoji: "🔥" };
+    case "grass":
+      return { borderColor: "#66CC66", emoji: "🌿" };
+    default:
+      return { borderColor: "#A0A0A0", emoji: "❓" };
+  }
+};
+
 export default function PokemonCard({
   name,
   image,
@@ -17,24 +32,36 @@ export default function PokemonCard({
   moves,
   weaknesses,
 }: PokemonCardProps) {
+  const { borderColor, emoji } = getTypeDetails(type);
   return (
     <View style={styles.card}>
-      <Text> {name}</Text>
-      <Text>{hp}</Text>
-      {/* 
-      <View>
-        <Image source={image} accessibilityLabel={name}></Image>
-      </View> */}
-
-      <View>
-        <Text>{type}</Text>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}> {name}</Text>
+        <Text style={styles.hp}>❤️{hp}</Text>
       </View>
 
       <View>
-        <Text>Moves: {moves.join(", ")}</Text>
+        <Image
+          style={styles.image}
+          source={image}
+          accessibilityLabel={name}
+        ></Image>
       </View>
-      <View>
-        <Text>Weaknesses: {weaknesses.join(", ")}</Text>
+
+      <View style={styles.typeContainer}>
+        <View style={[styles.badge, { borderColor }]}>
+          <Text style={styles.typeText}>{type}</Text>
+          <Text style={styles.typeEmoji}>{emoji}</Text>
+        </View>
+      </View>
+
+      <View style={styles.moveContainer}>
+        <Text style={styles.moveText}>Moves: {moves.join(", ")}</Text>
+      </View>
+      <View style={styles.weaknessContainer}>
+        <Text style={styles.weaknessText}>
+          Weaknesses: {weaknesses.join(", ")}
+        </Text>
       </View>
     </View>
   );
@@ -57,4 +84,51 @@ export const styles = StyleSheet.create({
       android: { elevation: 5 },
     }),
   },
+
+  typeContainer: { marginBottom: 40, alignItems: "center" },
+  badge: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4,
+  },
+
+  typeText: { fontSize: 22, fontWeight: "bold" },
+  typeEmoji: {
+    fontSize: 30,
+    marginRight: 12,
+  },
+
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 32,
+  },
+
+  name: {
+    fontSize: 30,
+  },
+
+  hp: {
+    fontSize: 22,
+  },
+
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
+    resizeMode: "contain",
+  },
+
+  moveContainer: {
+    marginBottom: 16,
+  },
+  moveText: { fontSize: 22, fontWeight: "bold" },
+
+  weaknessContainer: {
+    marginBottom: 16,
+  },
+  weaknessText: { fontSize: 22, fontWeight: "bold" },
 });
