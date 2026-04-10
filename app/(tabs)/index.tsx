fef7e2ecd89d0b69ea43a86b9120b3ebf87b1b7f
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2026-04-10 18:16:31 -0400

    feat: migrate pokemon list to SectionList using grouped data constants

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,48 +1,23 @@
-import PokemonCard from "@/components/PokemonCard";
-import { FlatList, StyleSheet, View } from "react-native";
+import { SectionList, StyleSheet, Text, View } from "react-native";
 
+import groupedSectionList from "../../constants/grouped-data.json";
 export default function App() {
-  const pokemons = [
-    {
-      name: "Charmander",
-      image: require("../../assets/images/charmander.png"),
-      type: "Fire",
-      hp: 39,
-      moves: ["Scratch", "Ember", "Growl", "Leer"],
-      weaknesses: ["Water", "Rock"],
-    },
-
-    {
-      name: "Squirtle",
-      image: require("../../assets/images/squirtle.png"), // Replace with the actual image path
-      type: "Water",
-      hp: 44,
-      moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
-      weaknesses: ["Electric", "Grass"],
-    },
-
-    {
-      name: "Bulbasaur",
-      image: require("../../assets/images/bulbasaur.png"), // Replace with the actual image path
-      type: "Grass",
-      hp: 45,
-      moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
-      weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
-    },
-    {
-      name: "Pikachu",
-      image: require("../../assets/images/pikachu.png"), // Replace with the actual image path
-      type: "Electric",
-      hp: 35,
-      moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
-      weaknesses: ["Ground"],
-    },
-  ];
   return (
     <View style={styles.container}>
-      <FlatList
-        data={pokemons}
-        renderItem={({ item }) => <PokemonCard key={item.name} {...item} />}
+      <SectionList
+        sections={groupedSectionList}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.cardText}>{item}</Text>
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+        SectionSeparatorComponent={() => <View style={{ height: 16 }} />}
+        renderSectionHeader={({ section }) => {
+          return <Text style={styles.sectionHedearText}>{section.type}</Text>;
+        }}
       />
     </View>
   );
@@ -53,5 +28,36 @@ export const styles = StyleSheet.create({
     flex: 1,
 
     backgroundColor: "#f5f5f5",
+  },
+  sectionHedearText: {
+    backgroundColor: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  card: {
+    padding: 16,
+    backgroundColor: "white",
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+
+  emptyLabel: {
+    fontSize: 32,
+    textAlign: "center",
+  },
+
+  headerText: {
+    fontSize: 32,
+    marginBottom: 32,
+    textAlign: "center",
+  },
+
+  footerText: {
+    fontSize: 32,
+    marginBottom: 32,
+    textAlign: "center",
   },
 });
---
+++ constants/pokemons.ts
@@ -0,0 +1,42 @@
+interface Pokemon {
+  name: string;
+  image: any;
+  type: string;
+  hp: number;
+  moves: string[];
+  weaknesses: string[];
+}
+export const pokemons: Pokemon[] = [
+  {
+    name: "Charmander",
+    image: require("../../assets/images/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+  },
+  {
+    name: "Squirtle",
+    image: require("../../assets/images/squirtle.png"), // Replace with the actual image path
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  },
+  {
+    name: "Bulbasaur",
+    image: require("../../assets/images/bulbasaur.png"), // Replace with the actual image path
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  },
+  {
+    name: "Pikachu",
+    image: require("../../assets/images/pikachu.png"), // Replace with the actual image path
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+  },
+];
